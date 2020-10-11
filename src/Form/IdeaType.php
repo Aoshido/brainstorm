d41a252b18d4b8cb86c536d779c511a9b1a121d4
Aoshido <nmonzon@cajadepagos.com>
2020-10-11 19:10:34 -0300

    Old form style

--- src/Form/IdeaType.php
+++ src/Form/IdeaType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class IdeaType extends AbstractType {
@@ -13,7 +14,7 @@ class IdeaType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
                 ->add('title', TextType::class)
-                ->add('description', TextType::class)
+                ->add('description', TextareaType::class)
 //                ->add('description', DateType::class)
         ;
     }
